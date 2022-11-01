0c1c4c659061a0ad7b218eb465e455278fc32308
SilvioGodinho <silvio.godinho08@gmail.com>
2022-10-31 21:12:17 -0300

    Add files the project

---
+++ perguntacinema.cs
@@ -0,0 +1,18 @@
+using System;
+
+//List<PerguntaCinema> perguntasCinemas;
+//        perguntasCinemas = perguntaSobreCinema();
+
+public class PerguntaCinema:PerguntaJogo{
+    
+     public PerguntaCinema():base()
+     {
+        
+     }
+
+     static List<PerguntaCinema> perguntaSobreCinema(){
+          List<PerguntaCinema> lista = new List<PerguntaCinema>();
+          return lista;
+     }
+
+}
---
+++ perguntajogo.cs
@@ -0,0 +1,20 @@
+using System;
+
+public class PerguntaJogo{
+
+    public PerguntaJogo(){
+        this.Pergunta = "";
+        this.Resposta = "";
+    }
+
+    public PerguntaJogo(String pergunta, String resposta){
+        this.Pergunta = pergunta;
+        this.Resposta = resposta;
+    }
+    public String Pergunta {get; set;}
+
+    public String Resposta {get; set;}
+
+
+
+}
